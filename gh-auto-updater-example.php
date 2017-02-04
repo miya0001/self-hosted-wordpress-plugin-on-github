a31309f020fe7329058e1a72ff76878ed0479bb3
miya0001 <miya@wpist.me>
2017-02-04 21:51:44 +0900

    v0.4.0

--- gh-auto-updater-example.php
+++ gh-auto-updater-example.php
@@ -7,7 +7,13 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     gh-auto-updater-example
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.4.0
  *
  * @package         Gh_Auto_Updater_Example
  */
+
+add_action( 'init', function(){
+	$gh_user = 'miya0001';
+	$gh_repo = 'gh-auto-updater-example';
+	new Miya\WP\GH_Auto_Update( $gh_user, $gh_repo, __FILE__ );
+} );
