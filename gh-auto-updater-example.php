c9576a8450300e2766da60d115f48aaeb402ce15
miya0001 <miya@wpist.me>
2017-02-05 05:20:09 +0900

    update dependencies

--- gh-auto-updater-example.php
+++ gh-auto-updater-example.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     gh-auto-updater-example
  * Domain Path:     /languages
- * Version:         2.0.0
+ * Version:         3.0.0
  *
  * @package         Gh_Auto_Updater_Example
  */
