01ce0751b754d6af216fad20131afbe0dfa65400
miya0001 <miya@wpist.me>
2017-02-04 21:11:04 +0900

    add plugin files

---
+++ gh-auto-updater-example.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Gh Auto Updater Example
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gh-auto-updater-example
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gh_Auto_Updater_Example
+ */
