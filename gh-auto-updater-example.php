d8fea4a80f4665500501df8b467dd651a731318f
miya0001 <miya@wpist.me>
2017-02-05 21:31:23 +0900

    some fix

--- gh-auto-updater-example.php
+++ gh-auto-updater-example.php
@@ -15,7 +15,8 @@
 require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
 add_action( 'init', function(){
+	$plugin_slug = plugin_basename( __FILE__ );
 	$gh_user = 'miya0001';
 	$gh_repo = 'gh-auto-updater-example';
-	new Miya\WP\GH_Auto_Update( $gh_user, $gh_repo, __FILE__ );
+	new Miya\WP\GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
 } );
