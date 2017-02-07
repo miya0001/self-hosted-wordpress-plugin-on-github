2cd64ce9f13ad0cf4451fd8d53864e4327f094e5
miya0001 <miya@wpist.me>
2017-02-08 06:08:02 +0900

    udpate composer

--- self-hosted-wordpress-plugin-on-github.php
+++ self-hosted-wordpress-plugin-on-github.php
@@ -18,5 +18,5 @@ add_action( 'init', function(){
 	$plugin_slug = plugin_basename( __FILE__ );
 	$gh_user = 'miya0001';
 	$gh_repo = 'self-hosted-wordpress-plugin-on-github';
-	new Miya\WP\GH_Auto_Update( $plugin_slug, $gh_user, $gh_repo );
+	new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
 } );
